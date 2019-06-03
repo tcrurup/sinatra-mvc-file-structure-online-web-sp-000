ff0babaeea52ef3bc9574de1be8f553bb6be6671
Anthony Rurup <tcrurup@gmail.com>
2019-06-03 17:15:50 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,10 @@
 class Dog 
   
-  @@all
+  @@all = []
   
   attr_accessor :name, :breed, :age
   
-  def initialize (name, breed, age)
+  def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
